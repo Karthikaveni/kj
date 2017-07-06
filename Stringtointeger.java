03330effd98156c44e891d5c6b2c9bd6dbf7dd5a
Karthikaveni <karthikaveni18@gmail.com>
2017-07-06 17:55:29 +0530

    Create Stringtointeger.java

---
+++ Stringtointeger.java
@@ -0,0 +1,14 @@
+# kj
+import java.util.*;
+public class Stringtointeger
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+String p=new String();
+p=sc.next();
+System.out.println(p);
+int n=Integer.parseInt(p);
+System.out.println("String to integer conversion:"+n);
+}
+}
